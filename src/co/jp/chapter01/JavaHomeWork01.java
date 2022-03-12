2ef76f1db028e7e6e3f2f9979dcf71fbbbb72442
Eric <itoatech.git@gmail.com>
2022-03-12 12:35:13 +0900

    プロジェクトの初期設定

---
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -0,0 +1,10 @@
+package co.jp.chapter01;
+
+public class JavaHomeWork01 {
+
+	static int num;
+
+	public static void main(String[] args) {
+	}
+
+}
---
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -0,0 +1,11 @@
+package co.jp.chapter02;
+
+public class JavaHomeWork02 {
+
+	public static void main(String[] args) {
+
+
+
+	}
+
+}
