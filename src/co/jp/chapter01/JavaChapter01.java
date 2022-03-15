2fc925556897ed5a35a91b43867f6b422dfc8386
Tuan <tuantuan0495@gmail.com>
2022-03-15 21:01:48 +0900

    20220315 chapte01 TuanTuan

--- src/co/jp/chapter01/JavaChapter01.java
+++ src/co/jp/chapter01/JavaChapter01.java
@@ -5,5 +5,19 @@ public class JavaChapter01 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("test");
+		
+		
+		byte b = 127;
+		
+		int i = 1234567890;
+		
+		short s = 12345;
+		
+		long l = 123456789012345l;
+		
+		
+		
+		
+		
 	}
 }
--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -5,6 +5,15 @@ public class JavaHomeWork01 {
 	static int num;
 
 	public static void main(String[] args) {
+		
+		
+		int i = 10;
+		int n = i%5;
+		System.out.println(n);
+		
+		
+		
+		
 	}
 
 }
