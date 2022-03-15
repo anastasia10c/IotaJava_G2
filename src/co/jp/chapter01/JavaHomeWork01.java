5e0baa7d262d1e7072e3e677f63148aa8267430d
Eric <itoatech.git@gmail.com>
2022-03-15 21:06:19 +0900

    20220315 homeworkサンプル Ri

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -5,6 +5,22 @@ public class JavaHomeWork01 {
 	static int num;
 
 	public static void main(String[] args) {
+
+		// 質問０１
+		// int i = 10;
+		// int n = i%5;
+
+		// N = XXXXX
+
+		// 質問２︓ 右の式を解釈してください: a = a > 10 ? 11 : 0;
+
+
+		// 質問３︓変数値を⽐較する演算⼦は = or == ?
+
+
+		// 質問４︓ブリアン（ boolean ）変数に設定可能の値は「?」「?」。
+
+
 	}
 
 }
